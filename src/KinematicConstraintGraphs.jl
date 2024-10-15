48a0230accc624f6ff29605a76cdc0c001e4339a
Sascha Stüttgen <sascha.steuttgen@rwth-aachen.de>
2024-10-15 13:07:07 +0200

    Package imports

--- src/KinematicConstraintGraphs.jl
+++ src/KinematicConstraintGraphs.jl
@@ -1,5 +1,11 @@
 module KinematicConstraintGraphs
 
+import AbstractAlgebra
+import Graphs
+using StaticArrays
+using LinearAlgebra
 # Write your package code here.
 
+include("Geometry.jl")
+include("EuclideanGroup.jl")
 end
