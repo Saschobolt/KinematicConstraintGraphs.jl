b05488283ccb9b123e65d17176f0309b93948f34
Sascha Stüttgen <sascha.steuttgen@rwth-aachen.de>
2024-10-22 12:07:30 +0200

    exports

--- src/KinematicConstraintGraphs.jl
+++ src/KinematicConstraintGraphs.jl
@@ -5,6 +5,9 @@ import Graphs
 using StaticArrays
 using LinearAlgebra
 # Write your package code here.
+export Point, GeometricSpan, Line, Plane, dim, basis, matrix, parallel, collinear, orthogonal
+export Abstract3dDisplacementGroup
+export IdentityGroup, SpecialEuclideanGroup, PrismaticGroup, RevolutionGroup, ScrewGroup, PlanarTranslationGroup, CylindricalGroup, SpatialTranslationGroup, PlanarSlidingGroup, SphericalRotationGroup, TranslatingScrewGroup, TranslatingGimbalGroup
 
 include("Geometry.jl")
 include("EuclideanGroup.jl")
