babcc25aeb25c4f2eaa887974459e0c9b13e8a7c
Sascha Stüttgen <sascha.steuttgen@rwth-aachen.de>
2024-10-22 12:07:20 +0200

    Displacement groups

--- src/EuclideanGroup.jl
+++ src/EuclideanGroup.jl
@@ -34,22 +34,217 @@ end
 # translation along arbitrary axis represented as a Line. The direction of axis is axis.points[2] - axis.points[1]
 translation(axis::Line, t::Real) = translation(axis.points[2] - axis.points[1], t)
 
+######################## displacement groups in R^3. See Graphs of kinematic constraints by Frederico Thomas, http://www.iri.upc.edu/people/thomas/papers/Computer-Aided.pdf
+"""
+Abstract supertype of all displacement groups. A displacement group is a group of transformations that act on a euclidean space.
+All subtypes of AbstractDisplacementGroup must have the following fields:
+- d::Int: dimension of euclidean space that the group acts on. E.g R^3 -> d = 3
+- dof::Int: degrees of freedom of the group. E.g SE(3) -> dof = 6
+- defining_object<:Union{Nothing, AbstractGeometricObject{N}}: object that defines the group. E.g. for translational group along a plane in R^3, defining_object is the defining line
+"""
+abstract type AbstractDisplacementGroup{N,T<:Real} <: AbstractAlgebra.Group end
+abstract type Abstract3dDisplacementGroup{T<:Real} <: AbstractDisplacementGroup{3,T} end
 
-abstract type AbstractDisplacementGroup{S,T<:Real} <: AbstractAlgebra.Group end
-struct DisplacementGroup{N,T<:Real} <: AbstractDisplacementGroup{N,T}
-    d::Int # dimension of euclidean space that the group acts on. E.g R^3 -> d = 3
-    dof::Int # degrees of freedom of the group. E.g SE(3) -> dof = 6
-    defining_objects::Vector{<:AbstractGeometricObject{N}}
-    # gens::Vector{DisplacementGroupElem{N,T}} # generators of the group as DisplacementGroupElem
+# identity group
+struct IdentityGroup{N,T<:Real} <: AbstractDisplacementGroup{N,T}
+    d::Int
+    dof::Int
+    defining_object::Nothing
+
+    function IdentityGroup{N,T}() where {N,T<:Real}
+        d = N
+        dof = 0
+        return new{N,T}(d, dof, nothing)
+    end
 end
+
+IdentityGroup(d::Integer=3) = IdentityGroup{d,Float64}()
+
+# special euclidean Group
+struct SpecialEuclideanGroup{N,T<:Real} <: AbstractDisplacementGroup{N,T}
+    d::Int
+    dof::Int
+    defining_object::Nothing
+
+    function SpecialEuclideanGroup{N,T}() where {N,T<:Real}
+        d = N
+        dof = binomial(N + 1, 2)
+        new{N,Float64}(d, dof, nothing)
+    end
+end
+
+SpecialEuclideanGroup(d::Integer=3) = SpecialEuclideanGroup{d,Float64}()
+
+# rectilinear translation (P) Prismatic
+struct PrismaticGroup{T<:Real} <: Abstract3dDisplacementGroup{T}
+    d::Int
+    dof::Int
+    defining_object::Line{3,T}
+
+    function PrismaticGroup(axis::Line{3,T}=Line(Point(0, 0, 0), Point(1, 0, 0))) where {T<:Real}
+        d = 3
+        dof = 1
+        new{T}(d, dof, axis)
+    end
+end
+
+PrismaticGroup(p1::Point{3}, p2::Point{3}) = PrismaticGroup(Line(p1, p2))
+PrismaticGroup(p::Point{3}, v::AbstractVector) = PrismaticGroup(Line(p, p + v))
+
+# Roation around an axis (R) Revolution
+struct RevolutionGroup{T<:Real} <: Abstract3dDisplacementGroup{T}
+    d::Int
+    dof::Int
+    defining_object::Line{3,T}
+
+    function RevolutionGroup(axis::Line{3,T}=Line(Point(0, 0, 0), Point(1, 0, 0))) where {T<:Real}
+        d = 3
+        dof = 1
+        new{T}(d, dof, axis)
+    end
+end
+
+RevolutionGroup(p1::Point{3}, p2::Point{3}) = RevolutionGroup(Line(p1, p2))
+RevolutionGroup(p::Point{3}, v::AbstractVector) = RevolutionGroup(Line(p, p + v))
+
+# Helicoidal movement (H) Screw
+struct ScrewGroup{T<:Real} <: Abstract3dDisplacementGroup{T}
+    d::Int
+    dof::Int
+    defining_object::Line{3,T}
+    pitch::Float64 # pitch of the screw
+
+    function ScrewGroup(axis::Line{3,T}=Line(Point(0, 0, 0), Point(1, 0, 0)), pitch::Real=1.0) where {T<:Real}
+        d = 3
+        dof = 1
+        new{T}(d, dof, axis, pitch)
+    end
+end
+
+ScrewGroup(p1::Point{3}, p2::Point{3}) = ScrewGroup(Line(p1, p2))
+ScrewGroup(p::Point{3}, v::AbstractVector, pitch::Real) = ScrewGroup(Line(p, p + v), pitch)
+
+# Planar translation
+struct PlanarTranslationGroup{T<:Real} <: Abstract3dDisplacementGroup{T}
+    d::Int
+    dof::Int
+    defining_object::Plane{3,T}
+
+    function PlanarTranslationGroup(plane::Plane{3,T}=Plane(Point(0, 0, 0), Point(0, 1, 0), Point(0, 0, 1))) where {T<:Real}
+        d = 3
+        dof = 2
+        new{T}(d, dof, plane)
+    end
+end
+
+PlanarTranslationGroup(p1::Point{3}, p2::Point{3}, p3::Point{3}) = PlanarTranslationGroup(Plane(p1, p2, p3))
+PlanarTranslationGroup(p::Point{3}, v1::AbstractVector, v2::AbstractVector) = PlanarTranslationGroup(Plane(p, p + v1, p + v2))
+
+# Lock movement (C) Cylindrical
+struct CylindricalGroup{T<:Real} <: Abstract3dDisplacementGroup{T}
+    d::Int
+    dof::Int
+    defining_object::Line{3,T}
+
+    function CylindricalGroup(axis::Line{3,T}=Line(Point(0, 0, 0), Point(1, 0, 0))) where {T<:Real}
+        d = 3
+        dof = 2
+        new{T}(d, dof, axis)
+    end
+end
+
+CylindricalGroup(p1::Point{3}, p2::Point{3}) = CylindricalGroup(Line(p1, p2))
+CylindricalGroup(p::Point{3}, v::AbstractVector) = CylindricalGroup(Line(p, p + v))
+
+# Spatial translation
+struct SpatialTranslationGroup{T<:Real} <: Abstract3dDisplacementGroup{T}
+    d::Int
+    dof::Int
+    defining_object::Nothing
+
+    function SpatialTranslationGroup{T}() where {T<:Real}
+        d = 3
+        dof = 3
+        new{T}(d, dof, nothing)
+    end
+end
+
+SpatialTranslationGroup() = SpatialTranslationGroup{Float64}
+
+# Planar sliding (E) Plane
+struct PlanarSlidingGroup{T<:Real} <: Abstract3dDisplacementGroup{T}
+    d::Int
+    dof::Int
+    defining_object::Plane{3,T}
+
+    function PlanarSlidingGroup(plane::Plane{3,T}=Plane(Point(0, 0, 0), Point(0, 1, 0), Point(0, 0, 1))) where {T<:Real}
+        d = 3
+        dof = 3
+        return new{T}(d, dof, plane)
+    end
+end
+
+PlanarSlidingGroup(p1::Point{3}, p2::Point{3}, p3::Point{3}) = PlanarSlidingGroup(Plane(p1, p2, p3))
+PlanarSlidingGroup(p::Point{3}, v1::AbstractVector, v2::AbstractVector) = PlanarSlidingGroup(Plane(p, p + v1, p + v2))
+
+# Spheric rotation (S) Spherical
+struct SphericalRotationGroup{T<:Real} <: Abstract3dDisplacementGroup{T}
+    d::Int
+    dof::Int
+    defining_object::Point{3,T}
+
+    function SphericalRotationGroup(point::Point{3,T}=Point(0, 0, 0)) where {T<:Real}
+        d = 3
+        dof = 3
+        return new{T}(d, dof, point)
+    end
+end
+
+# Translating Screw
+struct TranslatingScrewGroup{T<:Real} <: Abstract3dDisplacementGroup{T}
+    d::Int
+    dof::Int
+    defining_object::Line{3,T}
+    pitch::Real
+
+    function TranslatingScrewGroup(axis::Line{3,T}=Line(Point(0, 0, 0), Point(1, 0, 0)), pitch::Real=1) where {T<:Real}
+        d = 3
+        dof = 3
+        return new{T}(d, dof, axis, pitch)
+    end
+end
+
+TranslatingScrewGroup(p1::Point{3}, p2::Point{3}, pitch::Real) = TranslatingScrewGroup(Line(p1, p2), pitch)
+TranslatingScrewGroup(p::Point{3}, v::AbstractVector, pitch::Real) = TranslatingScrewGroup(Line(p, p + v), pitch)
+
+# Translating gimbal
+struct TranslatingGimbalGroup{T<:Real} <: Abstract3dDisplacementGroup{T}
+    d::Int
+    dof::Int
+    defining_object::Line{3,T}
+
+    function TranslatingGimbalGroup(axis::Line{3,T}=Line(Point(0, 0, 0), Point(1, 0, 0))) where {T<:Real}
+        d = 3
+        dof = 4
+        return new{T}(d, dof, axis)
+    end
+end
+
+######## subgroup relations among displacement groups
+
+TranslatingGimbalGroup(p1::Point{3}, p2::Point{3}) = TranslatingGimbalGroup(Line(p1, p2))
+TranslatingGimbalGroup(p::Point{3}, v::AbstractVector) = TranslatingGimbalGroup(Line(p, p + v))
+
+
+
 
 abstract type AbstractDisplacementGroupElem{S,T<:Real} <: AbstractAlgebra.GroupElem end
 
 struct DisplacementGroupElem{N,T<:Real} <: AbstractDisplacementGroupElem{N,T}
-    parent::DisplacementGroup{N,T}
+    # parent::DisplacementGroup{N,T}
     mat::Matrix{T}
 
-    function DisplacementGroupElem(group::DisplacementGroup{S,T}, mat::Matrix{T}) where {S,T<:Real}
+    function DisplacementGroupElem(group::AbstractDisplacementGroup{S,T}, mat::Matrix{T}) where {S,T<:Real}
         d = group.d
         @assert mat[d+1, d+1] == one(T) && det(mat[1:d, 1:d]) ≈ one(T) "mat must be in SE($d)."
 
@@ -60,4 +255,4 @@ end
 
 Base.parent(g::DisplacementGroupElem) = g.parent
 
-(G::DisplacementGroup)(mat::Matrix{<:Real}) = DisplacementGroupElem(G, mat)
+# (G::AbstractDisplacementGroup)(mat::Matrix{<:Real}) = DisplacementGroupElem(G, mat)
