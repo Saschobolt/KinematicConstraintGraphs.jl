839bc25df580c6a8482ab39ee5c387ce0fae15e6
Sascha Stüttgen <sascha.steuttgen@rwth-aachen.de>
2024-11-11 10:17:12 +0100

    filter constraints and find all equivalent constraints between a src vertex and all other vertices

--- src/KinematicConstraintGraphs.jl
+++ src/KinematicConstraintGraphs.jl
@@ -9,7 +9,7 @@ using LinearAlgebra
 export AbstractGeometricSpan, Point, GeometricSpan, Line, Plane, dim, basis, matrix, parallel, collinear, orthogonal, intersection
 export Abstract3dDisplacementGroup
 export inv!, IdentityGroup, SpecialEuclideanGroup, PrismaticGroup, RevolutionGroup, ScrewGroup, PlanarTranslationGroup, CylindricalGroup, SpatialTranslationGroup, PlanarSlidingGroup, SphericalRotationGroup, TranslatingScrewGroup, TranslatingGimbalGroup, is_subgroup, intersection
-export ConstraintGraph, add_vertex!, add_constraint!, blocks, filter_constraints!
+export ConstraintGraph, add_vertex!, add_constraint!, blocks, filter_constraints!, filter_constraints, all_equiv_constraints!, all_equiv_constraints, is_interlocking
 
 include("Geometry.jl")
 include("EuclideanGroup.jl")
--- src/constraint_graph.jl
+++ src/constraint_graph.jl
@@ -1,9 +1,10 @@
 mutable struct ConstraintGraph{N,T<:Integer}
     G::Graphs.SimpleDiGraph{T}
     constraints::Dict{Graphs.Edge{T},DisplacementGroupComposition{N}}
+    filtered::Bool # flag that indicates if the constraints have been filtered
 
     function ConstraintGraph(g::Graphs.SimpleDiGraph{T}, constraints::Dict{Graphs.Edge{T},DisplacementGroupComposition{N}}) where {N,T<:Integer}
-        new{N,T}(g, constraints)
+        new{N,T}(g, constraints, false)
     end
 end
 
@@ -33,6 +34,7 @@ Add a new vertex to the constraint graph `g`.
 """
 function add_vertex!(g::ConstraintGraph{N,T}) where {N,T<:Integer}
     Graphs.add_vertex!(g.G)
+    g.filtered = false
     return g
 end
 
@@ -44,6 +46,7 @@ Add a constraint between two vertices in the constraint graph `g`. Return true i
 function add_constraint!(g::ConstraintGraph{N,T}, src::T, dst::T, constraint::DisplacementGroupComposition{N}) where {N,T<:Integer}
     if Graphs.add_edge!(g.G, src, dst)
         g.constraints[Graphs.Edge(src, dst)] = constraint
+        g.filtered = false
         return true
     end
 
@@ -86,6 +89,10 @@ end
 Filter the constraints of the constraint graph g along the basis cycles of g. See Thomas 1991, section 4.4.3
 """
 function filter_constraints!(g::ConstraintGraph{N,T}) where {N,T<:Integer}
+    if g.filtered
+        return g
+    end
+
     B = blocks(g.G)
     filter!(b -> length(b) > 1, B) # remove bridges from the constraint graph as those constraints do not propagate through the graph
 
@@ -141,6 +148,11 @@ function filter_constraints!(g::ConstraintGraph{N,T}) where {N,T<:Integer}
             # check arc consistency
             key_vec = collect(keys(propagation_dict))
             updated_constraints = unique([x[2] for x in key_vec])
+
+            if length(updated_constraints) == 0
+                repeat = false
+            end
+
             for e in updated_constraints
                 # intersect constraint over all cycles it is a part of
                 sol = DisplacementGroupComposition([SpecialEuclideanGroup(N)])
@@ -161,8 +173,72 @@ function filter_constraints!(g::ConstraintGraph{N,T}) where {N,T<:Integer}
         end
     end
 
+    g.filtered = true
+
     return g
 end
+
+filter_constrints(g::ConstraintGraph{N,T}) where {N,T<:Integer} = filter_constraints!(deepcopy(g))
+
+
+"""
+    all_equiv_constraints(g::ConstraintGraph, src::Integer)
+
+Compute all equivalent constraints of the constraint graph g between the vertex src and all other vertices in the graph. 
+This is achieved by filtering the constraints in the constraint graph and then composing the constraints along shortest paths from src to the other vertices.
+The shortest paths are calculated using Dijkstra's algorithm.
+"""
+function all_equiv_constraints!(g::ConstraintGraph{N,T}, src::Integer) where {N,T<:Integer}
+    # filter constraints
+    filter_constraints!(g)
+
+    f = deepcopy(g)
+
+    # we are interested in paths that don't care about the direction of the edges -> add reversed edges with inverse constraints
+    for e in Graphs.edges(g.G)
+        add_constraint!(f, e.dst, e.src, inv(g.constraints[e]))
+    end
+
+    # compute the shortest paths
+    dijk = Graphs.dijkstra_shortest_paths(f.G, src)
+
+    # compute the equivalent constraints by following the paths computed by Dijkstra's algorithm
+    # this can be done in O(n) time 
+    equiv_constraints = [DisplacementGroupComposition([IdentityGroup(N)]) for _ in 1:Graphs.nv(f.G)]
+    for v in sort(collect(1:Graphs.nv(f.G)), by=v -> dijk.dists[v])
+        if dijk.dists[v] == 0
+            continue
+        end
+
+        # equivalent constraint is the composition of the equivalent constraint of the parent vertex of v along a shortest path with the filtered constraint of the edge connecting the parent vertex with v
+        equiv_constraints[v] = equiv_constraints[dijk.parents[v]] * f.constraints[Graphs.Edge(dijk.parents[v], v)]
+    end
+
+    return equiv_constraints
+end
+
+# function that checks if a kinematic constraint graph is interlocking when a given frame is fixed.
+# This is done by adding a global reference frame (a new vertex) and fixing the frame vertices relative to the global reference frame (by imposing constraints = I).
+# Then the equivalent constraints between the global reference frame and all other vertices are computed.
+# If all equivalent constraints are trivial, the constraint graph is interlocking. (The allowed motions of the blocks are a subset of the constraints).
+function is_interlocking(g::ConstraintGraph{N,T}, frame::AbstractVector{<:Integer}) where {N,T<:Integer}
+    if !issubset(frame, collect(1:Graphs.nv(g.G)))
+        throw(ArgumentError("Frame vertices are not a subset of the vertices of the constraint graph"))
+    end
+
+    f = deepcopy(g)
+
+    add_vertex!(f) # add vertex representing global reference frame
+    for v in frame
+        add_constraint!(f, Graphs.nv(f.G), v, DisplacementGroupComposition([IdentityGroup(N)])) # fix frame vertices relative to global reference frame
+    end
+
+    # check if the constraint graph is interlocking
+    equiv_constraints = all_equiv_constraints!(f, Graphs.nv(f.G))
+    return all(constraint -> is_trivial!(constraint) && constraint.factors[1] == IdentityGroup(N), equiv_constraints)
+end
+
+all_equiv_constraints(g::ConstraintGraph{N,T}, src::Integer) where {N,T<:Integer} = all_equiv_constraints!(deepcopy(g), src)
 
 #################### plotting of constraint graphs using GraphMakie
 function GraphMakie.graphplot(g::ConstraintGraph, kwargs...)
